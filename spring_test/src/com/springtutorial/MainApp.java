c5498b36774acee5450d5d90c29dec306a62a0d2
ycycorona@qq.com <ycy6323892>
2017-11-06 16:45:23 +0800

    初始化

---
+++ spring_test/src/com/springtutorial/MainApp.java
@@ -0,0 +1,11 @@
+package com.springtutorial;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class MainApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        Student student = (Student) context.getBean("student");
+        System.out.println("Name : " + student.getName() );
+        System.out.println("Age : " + student.getAge() );
+    }
+}
